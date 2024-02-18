a78a1eb8d5a57d0ed2262f788d203ce071c1726a
pheobeayo <pheobeayo@gmail.com>
2024-02-18 04:41:02 +0100

    modify-integration

---
+++ src/consts/contractAddresses.js
@@ -0,0 +1,6 @@
+export const stakingTokenContractAddress =
+  "0x64Cf8aB052073Cc810C6F3cC14D089a1313757ee";
+export const rewardsTokenContractAddress =
+  "0x08F3E6eD4CafBAfF253D0ABc0E90b7243cc08558";
+export const stakingContractAddress =
+  "0xfAC002195341F8E5A9535E29C2b621f113BDd8B0";
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { Notifications } from "@mantine/notifications";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { KryptoProvider } from "./context/KryptoContext.jsx";
 
-const network = "testnet";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -35,19 +35,19 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <ThirdwebProvider
           activeChain={{
             // === Required information for connecting to the network === \\
-            chainId: "0x128", // Chain ID of the network
+            chainId: "11155111", // Chain ID of the network
             // Array of RPC URLs to use
-            rpc: [`https://${network}.hashio.io/api`],
+            rpc: [`https://eth-sepolia.g.alchemy.com/v2/api`],
 
             // === Information for adding the network to your wallet (how it will appear for first time users) === \\
             // Information about the chains native currency (i.e. the currency that is used to pay for gas)
-            nativeCurrency: { name: "HBAR", symbol: "ℏℏ", decimals: 18 },
-            shortName: "Hbar", // Display value shown in the wallet UI
-            slug: "hedera", // Display value shown in the wallet UI
+            nativeCurrency: { name: "ETH", symbol: " SepoliaETH", decimals: 18 },
+            shortName: "ETH", // Display value shown in the wallet UI
+            slug: "sepolia", // Display value shown in the wallet UI
             testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
-            chain: "Hedera Testnet", // Name of the network
-            name: "Hedera Testnet", // Name of the network
-            explorers: [`https://hashscan.io/${network}/`], //Array of blockExplorers url to use
+            chain: "Sepolia Test Netwok", // Name of the network
+            name: "Sepolia Test Netwok", // Name of the network
+            explorers: [` https://sepolia.etherscan.io/`], //Array of blockExplorers url to use
           }}
         >
           <KryptoProvider>
